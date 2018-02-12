b3805d011aaa0e3a5a13afd7af7714e0b4e11709
David L <dv.lnh.d@gmail.com>
2018-02-12 12:52:35 +0100

    load more gl functions

--- game1gfx/glutil.cpp
+++ game1gfx/glutil.cpp
@@ -4,15 +4,33 @@
 #include <stdio.h>
 
 void (*glEnableVertexAttribArray)(GLuint index) = NULL;
+void(*glVertexAttribPointer)(GLuint index,
+	GLint size,
+	GLenum type,
+	GLboolean normalized,
+	GLsizei stride,
+	const GLvoid * pointer) = NULL;
 GLint (*glGetAttribLocation)(GLuint program, const char *name) = NULL;
 GLint(*glGetUniformLocation)(GLuint program, const char *name) = NULL;
 void(*glUseProgram)(GLuint program) = NULL;
+void(*glActiveTexture)(GLenum texture) = NULL;
+void(*glDeleteBuffers)(GLsizei n, const GLuint * buffers) = NULL;
+void(*glGenBuffers)(GLsizei n, GLuint * buffers) = NULL;
 
 void loadGLFunctions() {
 	glEnableVertexAttribArray = (void(*)(GLuint))wglGetProcAddress("glEnableVertexAttribArray");
+	glVertexAttribPointer = (void(*)(GLuint index,
+		GLint size,
+		GLenum type,
+		GLboolean normalized,
+		GLsizei stride,
+		const GLvoid * pointer))wglGetProcAddress("glVertexAttribPointer");
 	glGetAttribLocation = (GLint(*)(GLuint, const char*))wglGetProcAddress("glGetAttribLocation");
 	glGetUniformLocation = (GLint(*)(GLuint, const char*))wglGetProcAddress("glGetUniformLocation");
 	glUseProgram = (void(*)(GLuint))wglGetProcAddress("glUseProgram");
+	glActiveTexture = (void(*)(GLenum))wglGetProcAddress("glActiveTexture");
+	glDeleteBuffers = (void(*)(GLsizei, const GLuint*))wglGetProcAddress("glDeleteBuffers");
+	glGenBuffers = (void(*)(GLsizei, GLuint*))wglGetProcAddress("glGenBuffers");
 	
 	puts("Loaded gl functions");
 }
--- game1gfx/glutil.hpp
+++ game1gfx/glutil.hpp
@@ -6,6 +6,15 @@
 extern void loadGLFunctions();
 
 extern void(*glEnableVertexAttribArray)(GLuint index);
+extern void(*glVertexAttribPointer)(GLuint index,
+	GLint size,
+	GLenum type,
+	GLboolean normalized,
+	GLsizei stride,
+	const GLvoid * pointer);
 extern GLint(*glGetAttribLocation)(GLuint program, const char *name);
 extern GLint(*glGetUniformLocation)(GLuint program, const char *name);
 extern void(*glUseProgram)(GLuint program);
+extern void(*glActiveTexture)(GLenum texture);
+extern void(*glDeleteBuffers)(GLsizei n, const GLuint * buffers);
+extern void(*glGenBuffers)(GLsizei n, GLuint * buffers);
